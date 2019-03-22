d7fe3dd7da7b544b4fe994dfe6ce94fd57781e75
diogo0001 <diogo.t0001@gmail.com>
2019-03-22 12:07:13 -0300

    Deploy beta

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const port = process.env.PORT || 3000
+
+// carrega a pasta com todos os arquivos
+app.use(express.static('public'));
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname + './public/index.html'));
+});
+
+app.listen(port);
+console.log("Conectado à porta: "+port)
